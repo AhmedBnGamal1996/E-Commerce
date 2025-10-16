a79a71cf5cc36d0ff8558b49bf7b11d3691159af
Ahmed Gamal <AhmedBnGamal2018@gmail.com>
2025-10-16 05:05:06 +0300

    Video 5 - Product Controller

---
+++ Infrastructure/Presentation/Controllers/ProductsController.cs
@@ -0,0 +1,57 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+using ServicesAbstraction.Contracts;
+using Shared.Dtos;
+
+namespace Presentation.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]     // BaseUrl/api/WeatherForecast
+
+    public class ProductsController(IServiceManager _serviceManager) : ControllerBase 
+    {
+
+        // EndPoint ==> Get All Products
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductResultDto>>> GetAllProductsAsync()
+       => Ok ( await _serviceManager.ProductService.GetAllProductsAsync() ) ;
+
+
+        // EndPoint ==> Get All Brands
+
+        [HttpGet("Brands")]
+        public async Task<ActionResult<IEnumerable<BrandResultDto>>> GetAllBrandsAsync()
+       => Ok(await _serviceManager.ProductService.GetAllBrandsAsync());
+
+
+        // EndPoint ==> Get All Types
+
+        [HttpGet("Types")]
+        public async Task<ActionResult<IEnumerable<TypeResultDto>>> GetAllTypesAsync()
+       => Ok(await _serviceManager.ProductService.GetAllTypesAsync());
+
+
+        // EndPoint ==> Get Product By ID
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<ProductResultDto>> GetProductByIdAsync(int id)
+       => Ok(await _serviceManager.ProductService.GetProductByIdAsync(id));
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
