a8538e819c80e6fda604652ee583e3b961bb723d
Ahmed Gamal <AhmedBnGamal2018@gmail.com>
2025-10-16 03:34:43 +0300

    Video 1 - GenericRepository

---
+++ Core/ClassLibrary1/Contracts/IGenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Entities;
+
+
+namespace Domain.Contracts
+{
+    public interface IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+
+        // Get All 
+        Task<IEnumerable<TEntity>> GetAllAsync(bool asNoTracking = false);
+
+
+
+        // Get By Id
+        Task<TEntity?> GetByIdAsync(TKey id);
+
+
+        // Add
+
+        Task AddAsync(TEntity entity);
+
+
+        // Update
+        void Update(TEntity entity);
+
+
+
+        // Delete
+        void Delete(TEntity entity);
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
