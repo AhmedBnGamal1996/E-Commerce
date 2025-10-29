24741d0d882c48a704cc081a61e68b67dd4fec43
Ahmed Gamal <AhmedBnGamal2018@gmail.com>
2025-10-29 17:42:32 +0300

    Video 1 Order Entities

---
+++ Core/ClassLibrary1/Entities/OrderModule/Address.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.OrderModule
+{
+    public class Address
+    {
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Country { get; set; } = string.Empty;
+
+        public string City { get; set; } = string.Empty;
+
+        public string Street { get; set; } = string.Empty;
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Core/ClassLibrary1/Entities/OrderModule/Order.cs
@@ -0,0 +1,62 @@
+﻿using Domain.Entities.IdentityModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShippingAddress = Domain.Entities.OrderModule.Address;
+
+namespace Domain.Entities.OrderModule
+{
+    public class Order : BaseEntity<Guid> 
+    {
+
+        public string UserEmail { get; set; }  = string.Empty;
+
+        public ShippingAddress ShippingAddress { get; set; }
+
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>(); 
+
+        public OrderPaymentStatus PaymentStatus { get; set; } = OrderPaymentStatus.Pending;
+
+        public DeliveryMethod DeliveryMethod { get; set; }
+
+        public int? DeliveryMethodId { get; set; }
+
+        public decimal SubTotal { get; set; }
+
+        public DateTimeOffset OrderDate {  get; set; } = DateTimeOffset.UtcNow;
+
+        public string PaymentIntentId { get; set; } = string.Empty;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
