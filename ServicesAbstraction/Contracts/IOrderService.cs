2f628e1e6c412cce55812fbef466b4717522f0b9
Ahmed Gamal <AhmedBnGamal2018@gmail.com>
2025-10-29 19:17:53 +0300

    Video 3 - IOrderService & OrderProfile

---
+++ ServicesAbstraction/Contracts/IOrderService.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Entities.OrderModule;
+using Shared.Dtos.OrderModule;
+
+
+namespace ServicesAbstraction.Contracts
+{
+    public  interface IOrderService
+    {
+
+        // GetByID ==> Take Guid Id ==> Return OrderResult 
+
+        Task<OrderResult> GetOrderByIdAsync(Guid id);
+
+
+
+        // GetAllByEmail ==> Take String Email ===> Return Enumerable<OrderResult> 
+
+        Task<IEnumerable<OrderResult>> GetOrdersByEmailAsync(string userEmail);
+
+
+
+        // CreateOrder ==> Take OrderRequest , String Email  => Return OrderResult 
+
+        Task<OrderResult> CreateOrdersAsync(OrderRequest order , string userEmail);
+
+
+        /// GetDeliveryMethods ==> Return Enumerable<DeliveryMethodsResult> 
+
+        Task<IEnumerable<DeliveryMethodResult>> GetDeliveryMethodsAsync();
+
+
+
+
+
+
+    }
+}
---
+++ Shared/Dtos/OrderModule/OrderRequest.cs
@@ -0,0 +1,33 @@
+﻿using Shared.Dtos.OrderModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Domain.Entities.OrderModule
+{
+    public record OrderRequest
+    {
+
+        public string BasketId { get; init; } = string.Empty;
+
+        public AddressDto ShippingAddress { get; init; } 
+
+        public int DeliveryMethodId { get; init; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
